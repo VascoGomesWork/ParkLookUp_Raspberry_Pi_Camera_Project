088dfeda49b586827f5157527695320ed9f26ee0
VascoGomesWork <vasco.gomes2000@gmail.com>
2024-06-06 00:52:35 +0100

    Something Working

--- ParkLookUpRaspberryPi.py
+++ ParkLookUpRaspberryPi.py
@@ -1,10 +1,27 @@
 import cv2
 import pickle
 
-img = cv2.imread('ParLookUp_Park.jpg')
+img = cv2.imread('ParkLookUpPark.jpg')
+
+width, height = 450, 335
+pos_list = []
+
+def mouse_click(events, x, y, flags, params):
+    
+    if events == cv2.EVENT_LBUTTONDOWN:
+        pos_list.append(((x + 180), (y + 250)))
+
 
 while True:
-
-    cv2.rectangle(img, (100, 100), (200, 150), (255, 0, 255), 2)
-    cv2.imshow("Park Image", img)
+    #                    X    Y      X  - Y         Cor    Espessura
+    #cv2.rectangle(img, (300, 385), (750, 720), (255, 0, 255), 3)
+    
+    for pos in pos_list:
+        cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height), (255, 0, 255), 3)
+    
+    img_resized = cv2.resize(img, (600, 700))
+    cv2.imshow("ParkImage", img_resized)
+    
+    cv2.setMouseCallback("ParkImage", mouse_click)
+    
     cv2.waitKey(1)
