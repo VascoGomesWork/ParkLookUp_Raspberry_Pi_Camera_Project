7adf5c20c19b0d0a01e3ea697854641382bda78e
VascoGomesWork <vasco.gomes2000@gmail.com>
2024-06-07 01:06:33 +0100

    Adding and Removing Park Positions

--- ParkLookUpRaspberryPi.py
+++ ParkLookUpRaspberryPi.py
@@ -1,26 +1,41 @@
 import cv2
 import pickle
 
-img = cv2.imread('ParkLookUpPark.jpg')
+width, height = 125, 98
 
-width, height = 450, 335
-pos_list = []
+try:
+    with open("ParkLookUpParkPositions", "rb") as f:
+        #Gets the Image Marks from the previous Run and Shows
+        pos_list = pickle.load(f)
+except:
+    pos_list = []
 
 def mouse_click(events, x, y, flags, params):
     
     if events == cv2.EVENT_LBUTTONDOWN:
-        pos_list.append(((x + 180), (y + 250)))
+        pos_list.append((x, y))
+        
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(pos_list):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                pos_list.pop(i)
+                
+    with open("ParkLookUpParkPositions", "wb") as f:
+        pickle.dump(pos_list, f)
 
 
 while True:
+    #In order to delete the rectangles the image needs to be constantly generated
+    img = cv2.imread('ParkLookUpPark2.jpg')
+    
     #                    X    Y      X  - Y         Cor    Espessura
-    #cv2.rectangle(img, (300, 385), (750, 720), (255, 0, 255), 3)
+    #cv2.rectangle(img, (45, 37), (170, 135), (255, 0, 255), 3)
     
     for pos in pos_list:
         cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height), (255, 0, 255), 3)
     
-    img_resized = cv2.resize(img, (600, 700))
-    cv2.imshow("ParkImage", img_resized)
+    cv2.imshow("ParkImage", img)
     
     cv2.setMouseCallback("ParkImage", mouse_click)
     
---
+++ main.py
@@ -0,0 +1,7 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+
+#Video Feed
+cap = cv2.VideoCapture("carPark.mp4")
